7199cb7f07fc3388f7c8a0db1e995c8bbba9e255
eren okur <32430029+erenokur@users.noreply.github.com>
2023-06-29 22:53:08 +0300

    async operations added to improve performance

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -5,6 +5,7 @@ using dotnet_3D_store_backend.Models;
 using dotnet_3D_store_backend.Helpers;
 using dotnet_3D_store_backend.Entities;
 using dotnet_3D_store_backend.Enumerators;
+using Microsoft.EntityFrameworkCore;
 
 public class ProductService
 {
@@ -54,25 +55,28 @@ public class ProductService
     {
         return _dbContext.Products.Find(id);
     }
-    public IEnumerable<Products> GetSuggestedProducts(SuggestedProductsRequest model)
+
+    // I changed this method to async need to be tested after some orders are created
+    public async Task<IEnumerable<Products>> GetSuggestedProductsAsync(SuggestedProductsRequest model)
     {
         int calculateIndex = model.CurrentPage * _appSettings.PageSize;
         // Get all user orders
-        var userOrders = _dbContext.Orders.Where(o => o.CostumerUserId == model.UserId);
+        var userOrders = await _dbContext.Orders.Where(o => o.CostumerUserId == model.UserId).ToListAsync();
         // Get all user order items
-        var userOrderItems = _dbContext.OrderItems.Where(oi => userOrders.Any(o => o.Id == oi.OrderId));
+        var userOrderItems = await _dbContext.OrderItems.Where(oi => userOrders.Any(o => o.Id == oi.OrderId)).ToListAsync();
         // Get all user products
-        var userProducts = _dbContext.Products.Where(p => userOrderItems.Any(oi => oi.ProductId == p.Id));
+        var userProducts = await _dbContext.Products.Where(p => userOrderItems.Any(oi => oi.ProductId == p.Id)).ToListAsync();
         // Get all user categories
         var userCategories = userProducts.Select(p => p.Category).Distinct();
         // Calculate the most popular category
         var mostPopularCategory = userCategories.GroupBy(c => c).OrderByDescending(c => c.Count()).First().Key;
         // Get all products from the most popular category
-        var suggestedProducts = _dbContext.Products
+        var suggestedProducts = await _dbContext.Products
             .Where(p => p.Category == mostPopularCategory)
             .Skip(calculateIndex)
-            .Take(_appSettings.PageSize);
-        return suggestedProducts.Any() ? suggestedProducts : _dbContext.Products.Skip(calculateIndex).Take(_appSettings.PageSize);
+            .Take(_appSettings.PageSize)
+            .ToListAsync();
+        return suggestedProducts.Any() ? suggestedProducts : await _dbContext.Products.Skip(calculateIndex).Take(_appSettings.PageSize).ToListAsync();
     }
 
     public IEnumerable<Products> GetProductsByCategory(ProductsByCategoryRequest model)
@@ -80,9 +84,15 @@ public class ProductService
         int calculateIndex = model.CurrentPage * _appSettings.PageSize;
         return _dbContext.Products.Where(p => p.Category == model.Category).Skip(calculateIndex).Take(_appSettings.PageSize);
     }
-    public IEnumerable<Products> GetProductsBySearch(ProductsBySearchRequest model)
+    public async Task<IEnumerable<Products>> GetProductsBySearch(ProductsBySearchRequest model)
     {
         int calculateIndex = model.CurrentPage * _appSettings.PageSize;
-        return _dbContext.Products.Where(p => p.Name.Contains(model.Search)).Skip(calculateIndex).Take(_appSettings.PageSize);
+        var products = await _dbContext.Products
+            .Where(p => p.Name.Contains(model.Search))
+            .Skip(calculateIndex)
+            .Take(_appSettings.PageSize)
+            .ToListAsync();
+
+        return products;
     }
 }
